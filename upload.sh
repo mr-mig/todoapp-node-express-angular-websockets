5e3b493c3666284b689edf525dfcb6d8cd0c456a
Alexey Migutsky <alexey.migutsky@gmail.com>
2014-06-26 15:39:54 +0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,9 +3,9 @@
 
 # $1 is a hostname, e.g. ubuntu@mrmig.info
 tar -cvzf nodeserver.tar.gz ./
-scp -i ~/.ssh/id_rsa -r nodeserver.tar.gz ubuntu@toptal.mrmig.info:/usr/local/nodejs/toptal
+scp -i ~/.ssh/id_rsa -r nodeserver.tar.gz ubuntu@test.mrmig.info:/usr/local/nodejs/testapp
 ssh -i ~/.ssh/id_rsa $1 << 'ENDSSH'
-cd /usr/local/nodejs/toptal
+cd /usr/local/nodejs/testapp
 tar -xvf nodeserver.tar.gz
 sudo cp nodeserver.conf /etc/init/
 sudo service nodeserver restart
